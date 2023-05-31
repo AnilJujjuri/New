2d8327d9dcd17fa2c00dd8dd556bbdf05e1e3743
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-05-31 12:37:05 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,20 +1,5 @@
-Error: MonitoredItemCreateRequest.__init__() got an unexpected keyword argument 'subscription'
-ServiceFault from server received while waiting for publish response
-exception calling callback for <Future at 0x20606c23310 state=finished returned Buffer>
-Traceback (most recent call last):
-  File "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0\lib\concurrent\futures\_base.py", line 342, in _invoke_callbacks
-    callback(self)
-  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\opcua\client\ua_client.py", line 493, in _call_publish_callback
-    self._uasocket.check_answer(data, "while waiting for publish response")
-  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\opcua\client\ua_client.py", line 93, in check_answer
-    hdr.ServiceResult.check()
-  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\opcua\ua\uatypes.py", line 218, in check
-    raise UaStatusCodeError(self.value)
-opcua.ua.uaerrors._auto.BadSessionClosed: "The session was closed by the client."(BadSessionClosed)
-Client disconnected
-from opcua import Client, ua, uamethod
-from opcua.ua import NodeId, ua_binary as uabin
-
+from opcua import Client, ua
+from opcua.ua import NodeId
 def subscribe_to_values(subscription, data_change_notification):
     print("Data change from server:")
     for item in data_change_notification.MonitoredItems:
@@ -22,19 +7,9 @@ def subscribe_to_values(subscription, data_change_notification):
         data_value = item.Value.Value
         print(f"Item {node_id}, Value = {data_value}")
 
-def on_session_closed(session, reason):
-    print(f"Session closed: {reason}")
-    # Add any necessary handling here
-
 if __name__ == '__main__':
     client = Client("opc.tcp://MYTSL02946.lnties.com:53530/OPCUA/SimulationServer")
-    
     try:
-        client.set_security_string("Basic256Sha256,SignAndEncrypt,certificate.pem,private-key.pem")
-        client.application_uri = "urn:RustClient"
-        
-        client.on_session_closed = on_session_closed
-        
         client.connect()
         print("Client connected")
         
@@ -59,8 +34,8 @@ if __name__ == '__main__':
         client.create_monitored_items(subscription, items_to_create)
         
         # Write a value to a node
-        node_id = NodeId(3, 1012)
-        value = uabin.Variant(20, uabin.VariantType.Int32)
+        node_id = ua.NodeId(3, 1012)
+        value = ua.Variant(20, ua.VariantType.Int32)
         client.write_attribute_value(node_id, ua.AttributeIds.Value, value)
         print("Value written successfully")
         
