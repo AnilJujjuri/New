db38a768db9023f472d2c8ab5881e14bbf678a9c
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-05-31 12:23:04 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -12,3 +12,68 @@ Traceback (most recent call last):
     raise UaStatusCodeError(self.value)
 opcua.ua.uaerrors._auto.BadSessionClosed: "The session was closed by the client."(BadSessionClosed)
 Client disconnected
+from opcua import Client, ua, uamethod
+from opcua.ua import NodeId, ua_binary as uabin
+
+def subscribe_to_values(subscription, data_change_notification):
+    print("Data change from server:")
+    for item in data_change_notification.MonitoredItems:
+        node_id = item.ClientHandle
+        data_value = item.Value.Value
+        print(f"Item {node_id}, Value = {data_value}")
+
+def on_session_closed(session, reason):
+    print(f"Session closed: {reason}")
+    # Add any necessary handling here
+
+if __name__ == '__main__':
+    client = Client("opc.tcp://MYTSL02946.lnties.com:53530/OPCUA/SimulationServer")
+    
+    try:
+        client.set_security_string("Basic256Sha256,SignAndEncrypt,certificate.pem,private-key.pem")
+        client.application_uri = "urn:RustClient"
+        
+        client.on_session_closed = on_session_closed
+        
+        client.connect()
+        print("Client connected")
+        
+        # Create a subscription
+        subscription = client.create_subscription(2000, ua.SubscriptionDiagnosticsDataType())
+        
+        # Create some monitored items
+        items_to_create = [
+            ua.MonitoredItemCreateRequest(
+                NodeId(3, 1003), ua.AttributeIds.Value, client_handle=1
+            ),
+            ua.MonitoredItemCreateRequest(
+                NodeId(3, 1008), ua.AttributeIds.Value, client_handle=2
+            ),
+            ua.MonitoredItemCreateRequest(
+                NodeId(3, 1009), ua.AttributeIds.Value, client_handle=3
+            ),
+            ua.MonitoredItemCreateRequest(
+                NodeId(3, 1010), ua.AttributeIds.Value, client_handle=4
+            )
+        ]
+        client.create_monitored_items(subscription, items_to_create)
+        
+        # Write a value to a node
+        node_id = NodeId(3, 1012)
+        value = uabin.Variant(20, uabin.VariantType.Int32)
+        client.write_attribute_value(node_id, ua.AttributeIds.Value, value)
+        print("Value written successfully")
+        
+        # Subscribe to data change notifications
+        subscription.subscribe_data_change(subscribe_to_values)
+        
+        # Keep the script running to receive data change notifications
+        while True:
+            pass
+    
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    finally:
+        client.disconnect()
+        print("Client disconnected")
