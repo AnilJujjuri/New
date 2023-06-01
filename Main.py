b2e21d4c5a7d918ef2c5089bb85f3d61f8566e2e
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-01 15:41:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -96,3 +96,33 @@ AttributeError: module 'opcua.ua' has no attribute 'Subscription'. Did you mean:
   File "d:\OneDrive - LTTS\Desktop\opc\opcrust.py", line 6, in subscribe_to_values
     subscription = session.create_subscription(session,100, opcua.subscription.SubscriptionDiagnosticsDataType())
 AttributeError: 'CreateSessionResult' object has no attribute 'create_subscription'
+    
+    import opcua
+
+def subscribe_to_values(session):
+    # Create a subscription
+    subscription = session.create_subscription(100, opcua.subscription.SubscriptionDiagnosticsDataType())
+    # Create a monitored item
+    node = session.get_node("ns=2;s=Temperature")
+    handle = subscription.subscribe_data_change(node)
+    return subscription, handle
+
+if __name__ == "__main__":
+    client = opcua.Client("opc.tcp://MYTSL02946.lnties.com:53530/OPCUA/SimulationServer")
+    client.set_security_string("None,Basic256,None,None")
+    client.connect()
+    print("Client created")
+    session = client.create_session()
+    print("Session created")
+    subscription, handle = subscribe_to_values(session)
+    print("Subscribed")
+    try:
+        while True:
+            pass
+    finally:
+        subscription.unsubscribe(handle)
+        session.close()
+        client.disconnect()
+
+    
+    
