ae35a50d2067b9e511a18cc284c3614cb29809dc
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-01 15:12:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,89 +1,34 @@
-from opcua import Client, ua
+import opcua
+from opcua import ua
 
+def subscribe_to_values(session):
+    # Create a subscription
+    subscription = session.create_subscription(100, ua.SubscriptionDiagnosticsDataType())
+    # Create a monitored item
+    node = session.get_node("ns=2;s=Temperature")
+    handle = subscription.subscribe_data_change(node)
+    return subscription, handle
 
-def data_change_handler(subscription, data):
-    print("Data change from server:")
-    for item in data.MonitoredItems:
-        node_id = item.MonitoredItemId
-        value = item.Value.Value
-        print(f"Item {node_id}, Value = {value}")
-
-
-if __name__ == '__main__':
-    client = Client("opc.tcp://MYTSL02946.lnties.com:53530/OPCUA/SimulationServer")
+if __name__ == "__main__":
+    client = opcua.Client("opc.tcp://MYTSL02946.lnties.com:53530/OPCUA/SimulationServer")
+    client.set_security_string("None")
+    client.connect()
+    print("Client created")
+    session = client.create_session()
+    print("Session created")
+    subscription, handle = subscribe_to_values(session)
+    print("Subscribed")
     try:
-        client.connect()
-        print("Client connected")
-
-        # Create a subscription with a data change handler
-        subscription = client.create_subscription(2000, data_change_handler)
-
-        # Create some monitored items
-        items_to_create = [
-            ua.MonitoredItemCreateRequest(
-                ItemToMonitor=ua.ReadValueId(
-                    NodeId=ua.NodeId(3, 1003),
-                    AttributeId=ua.AttributeIds.Value
-                ),
-                MonitoringMode=ua.MonitoringMode.Reporting,
-                RequestedParameters=ua.MonitoringParameters(
-                    ClientHandle=1,
-                    SamplingInterval=1000,
-                    QueueSize=10,
-                    DiscardOldest=True
-                )
-            ),
-            ua.MonitoredItemCreateRequest(
-                ItemToMonitor=ua.ReadValueId(
-                    NodeId=ua.NodeId(3, 1008),
-                    AttributeId=ua.AttributeIds.Value
-                ),
-                MonitoringMode=ua.MonitoringMode.Reporting,
-                RequestedParameters=ua.MonitoringParameters(
-                    ClientHandle=2,
-                    SamplingInterval=1000,
-                    QueueSize=10,
-                    DiscardOldest=True
-                )
-            ),
-            ua.MonitoredItemCreateRequest(
-                ItemToMonitor=ua.ReadValueId(
-                    NodeId=ua.NodeId(3, 1009),
-                    AttributeId=ua.AttributeIds.Value
-                ),
-                MonitoringMode=ua.MonitoringMode.Reporting,
-                RequestedParameters=ua.MonitoringParameters(
-                    ClientHandle=3,
-                    SamplingInterval=1000,
-                    QueueSize=10,
-                    DiscardOldest=True
-                )
-            ),
-            ua.MonitoredItemCreateRequest(
-                ItemToMonitor=ua.ReadValueId(
-                    NodeId=ua.NodeId(3, 1010),
-                    AttributeId=ua.AttributeIds.Value
-                ),
-                MonitoringMode=ua.MonitoringMode.Reporting,
-                RequestedParameters=ua.MonitoringParameters(
-                    ClientHandle=4,
-                    SamplingInterval=1000,
-                    QueueSize=10,
-                    DiscardOldest=True
-                )
-            )
-        ]
-
-        # Create the monitored items
-        monitored_items = subscription.create_monitored_items(items_to_create)
-
-        # Keep the script running to receive data change notifications
         while True:
             pass
-
-    except Exception as e:
-        print(f"Error: {e}")
-
     finally:
+        subscription.unsubscribe(handle)
+        session.close()
         client.disconnect()
-        print("Client disconnected")
+        
+        Traceback (most recent call last):
+  File "d:\OneDrive - LTTS\Desktop\opc\opcrust.py", line 14, in <module>
+    client.set_security_string("None")
+  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\opcua\client\client.py", line 188, in set_security_string
+    raise ua.UaError('Wrong format: `{0}`, expected at least 4 comma-separated values'.format(string))
+opcua.ua.uaerrors._base.UaError: Wrong format: `None`, expected at least 4 comma-separated values
